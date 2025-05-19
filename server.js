60aa805ca99c6f76530c356593687b5f387e65b7
expras <info@expras.com>
2025-05-19 22:53:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const createPayment = require("./create-payment");
+
+const createPaymentRoute = require("./create-payment");
+const confirmRoute = require("./sendEmail"); // make sure this is exported correctly
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post("/create-payment", createPayment);
+app.use("/create-payment", createPaymentRoute);
+app.use("/confirm", confirmRoute); // add this line if not yet added
 
 app.get("/", (req, res) => {
-  res.send("EXPRAS backend is running");
+  res.send("✅ EXPRAS backend is running");
 });
 
 const PORT = process.env.PORT || 3000;
